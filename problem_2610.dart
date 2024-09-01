227bbb8fc1ab1bbc80e4d415d87d847cac6282fe
J-L-Moreno <jose.moreno@encora.com>
2024-08-31 18:18:24 -0700

    Update problem_2610.dart

--- problem_2610.dart
+++ problem_2610.dart
@@ -2,23 +2,16 @@ class Solution {
   List<List<int>> findMatrix(List<int> nums) {
     List<List<int>> matrix = [];
     while(nums.isNotEmpty){
-      List<int> row = [];
+      List<int> temporaryRow = [];
       for(int i = 0; i < nums.length; i++){
-        if(row.isEmpty){
-            row.add(nums[i]);
+        if(!temporaryRow.contains(nums[i])){
+            temporaryRow.add(nums[i]);
             nums.removeAt(i);
             i--;
-        } else {
-            if(!row.contains(nums[i])){
-                row.add(nums[i]);
-                nums.removeAt(i);
-                i--;
-            }
         }
       }
-      matrix.add(row);
+      matrix.add(temporaryRow);
     }
-    print('hello');
     return matrix;
   }
 }
